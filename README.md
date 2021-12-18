1f089ce1fcf6e22043a8c7c6d0e4fa9cc1c70e74
Andres Hernandez <cybereality@gmail.com>
2021-12-17 23:21:01 -0800

    Added add-on files and readme

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# godot-super-scaling
+# Godot Super Scaling : Add-On For Godot Engine
+
 Godot Super Scaling, a comprehensive post-processing effect for 3D and 2D Godot Engine games that allows you to upscale or downscale the viewport for increased performance or higher quality visuals. Supports up to 200% native resolution or down to 10%.
+
+![Screenshot](ScreenShots/Godot_Super_Scaling_3D.jpg)
+
+![Screenshot](ScreenShots/Godot_Super_Scaling_2D.jpg)
+
+## INSTALLATION
+
+Please download the add-on via the official Godot AssetLib inside the Godot Editor.
+
+## CONFIGURATION
+
+* Add the `SuperScaling.tscn` scene to your main scene tree, it should be just below the root node.
+* Place your entire game world as a child of a new node (e.g. Spatial for 3D games or Node2D for 2D games).
+* In the `SuperScaling` inspector settings, link the `Game World` property to the node you created above.
+* Select `Enable on Play`, this will start the scaling add-on when you play the game (cannot be viewed in editor).
+* Set `Usage` to either `3D` or `2D` depending on your game type.
+* `MSAA` and `FXAA` are controlled by the `SuperScaling` add-on and the project settings do not take effect.
+* The `SuperScaling` add-on does add some anti-aliasing, however you may want to also enable `MSAA` or `FXAA` or both.
+* `Shadow Atlas` controls the map size for omni and spot lights. If you only use a directional light you can set this to `1`.
+* To control the scale of the render, use the `Scale Factor` setting on the top of the inspector for `SuperScaling`.
+* `1.0` for the `Scale Factor` corresponds to 100% of the native resolution. `0.5` will be 50% and `2.0` will be 200%.
+* Note that setting the `Scale Factor` above `1.0` results in higher quality but will have lesser performance.
+* Setting the `Scale Factor` lower than `1.0` results in an image quality loss, but higher performance.
+* Please experiment with your game and your desired minimum spec to find an appropriate setting.
+* Also consider exposing the scale to users, so that they may tailor the experience to their level of hardware.
+* `Smoothness` controls the mix between the two scaling algorithms. `0.0` is more sharp and `1.0` is softer.
+* In general, values lower than `0.5` are better for upscaling and values above `0.5` better for downscaling.
+* However, this is an artistic choice and you should experiment to find the best value for your game.
+* In the project settings, for 3D games, the `Stretch Mode` should set to `disabled` and `Stretch Aspect` to `ignore`.
+* For 2D games, the best project settings are `2d` for `Stretch Mode` and `keep_height` for `Stretch Aspect`.
+* 2D HUD and UI elements will be shown above the scaled game and displayed at native resolution.
+* For 3D games, simply create a 2D node (such as Control) as a child of the root node and it will display properly.
+* For 2D games, you will need to create a Node2D and place your UI elements inside that. Then set the `Z Index` to `1` or above.
+* One thing to note, `SuperScaling` will take control of your game while enabled, and you will no longer be able to edit as you play.
+* It is recommended to leave `Enable on Play` off while developing, and only enable the add-on when you need to test the graphics or performance.
+
+## LICENSE
+
+MIT License
+
+Copyright (c) 2021 Andres Hernandez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ SuperScaling/SuperScaling.gd
@@ -0,0 +1,240 @@
+# [Godot Super Scaling]
+# created by Andres Hernandez
+extends Node
+
+export (float, 0.1, 2.0) var scale_factor = 1.0 setget change_scale_factor
+export (float, 0.0, 1.0) var smoothness = 0.5 setget change_smoothness
+export (bool) var enable_on_play = false
+export (NodePath) var game_world
+export (int, "3D", "2D") var usage = 0
+export (int, "Disabled", "2X", "4X", "8X", "16X") var msaa = 0 setget change_msaa
+export (bool) var fxaa = false setget change_fxaa
+export (int, 1, 4096) var shadow_atlas = 4096 setget change_shadow_atlas
+onready var sampler_shader = preload("res://SuperScaling/SuperScaling.gdshader")
+var sampler_material
+var game_node
+var overlay
+var viewport
+var viewport_size
+var root_viewport
+var native_resolution
+var original_resolution
+var native_aspect_ratio
+var original_aspect_ratio
+enum {USAGE_3D, USAGE_2D}
+const epsilon = 0.01
+
+func _ready():
+	if (enable_on_play):
+		game_node = get_node(game_world)
+		if game_node:
+			get_parent().call_deferred("remove_child", game_node)
+			get_screen_size()
+			create_viewport()
+			set_shader_texture()
+			viewport.call_deferred("add_child", game_node)
+			get_parent().call_deferred("add_child", viewport)
+			original_resolution = native_resolution
+			original_aspect_ratio = native_aspect_ratio
+			root_viewport = get_viewport()
+			viewport.connect("size_changed", self, "on_window_resize")
+			root_viewport.connect("size_changed", self, "on_window_resize")
+			on_window_resize()
+			create_sampler()
+			change_msaa(msaa)
+			change_fxaa(fxaa)
+			change_smoothness(smoothness)
+			set_process_input(true)
+			set_process_unhandled_input(true)
+		else:
+			print("ERROR [Godot Super Scaling] Game World must be set in inspector.")
+	
+func create_viewport():
+	viewport = Viewport.new()
+	viewport.name = "Viewport"
+	viewport.size = native_resolution
+	viewport.usage = Viewport.USAGE_3D if usage == USAGE_3D else Viewport.USAGE_2D
+	viewport.render_target_clear_mode = Viewport.CLEAR_MODE_NEVER
+	viewport.render_target_update_mode = Viewport.UPDATE_ALWAYS
+	viewport.render_target_v_flip = true
+	viewport.size_override_stretch = true
+	viewport.msaa = Viewport.MSAA_DISABLED
+	viewport.shadow_atlas_size = shadow_atlas
+	
+func create_sampler():
+	overlay = ColorRect.new()
+	overlay.name = "SamplerOverlay"
+	sampler_material = ShaderMaterial.new()
+	sampler_material.shader = sampler_shader
+	overlay.material = sampler_material
+	add_child(overlay)
+
+func set_shader_texture():
+	yield(VisualServer, "frame_post_draw")
+	var view_texture = viewport.get_texture()
+	view_texture.flags = 0
+	view_texture.viewport_path = viewport.get_path()
+	sampler_material.set_shader_param("viewport", view_texture)
+	change_scale_factor(scale_factor)
+	
+func set_shader_resolution():
+	if sampler_material:
+		sampler_material.set_shader_param("view_resolution", viewport_size)
+	
+func get_screen_size():
+	var window = OS.window_size
+	native_resolution = window
+	native_aspect_ratio = native_resolution.x / native_resolution.y
+
+func set_viewport_size():
+	var res_float = native_resolution * scale_factor
+	viewport_size = Vector2(round(res_float.x), round(res_float.y))
+	var aspect_setting = get_aspect_setting()
+	if native_aspect_ratio and original_aspect_ratio and (aspect_setting != "ignore" and aspect_setting != "expand"):
+		var aspect_diff = native_aspect_ratio / original_aspect_ratio
+		if usage == USAGE_2D:
+			if aspect_diff > 1.0 + epsilon and aspect_setting == "keep_width":
+				viewport_size = Vector2(round(res_float.y * native_aspect_ratio), round(res_float.y))
+			elif aspect_diff < 1.0 - epsilon and aspect_setting == "keep_height":
+				viewport_size = Vector2(round(res_float.x), round(res_float.y / native_aspect_ratio))	
+		elif usage == USAGE_3D:
+			if aspect_diff > 1.0 + epsilon:
+				viewport_size = Vector2(round(res_float.x / aspect_diff), round(res_float.y))
+			elif aspect_diff < 1.0 - epsilon:
+				viewport_size = Vector2(round(res_float.x), round(res_float.y * aspect_diff))
+	
+func resize_viewport():
+	if viewport:
+		viewport.size = viewport_size
+			
+func scale_viewport_canvas():
+	if viewport:
+		var aspect_setting = get_aspect_setting()
+		var aspect_diff = native_aspect_ratio / original_aspect_ratio
+		if aspect_setting == "ignore":
+			viewport.set_size_override(true, original_resolution)
+		elif aspect_setting == "expand":
+			viewport.set_size_override(true, native_resolution)
+		else:
+			if usage == USAGE_2D:
+				if aspect_diff < 1.0 - epsilon and aspect_setting == "keep_width":
+					viewport.set_size_override(true, Vector2(round(original_resolution.x), round(original_resolution.x / native_aspect_ratio)))
+				elif aspect_diff > 1.0 + epsilon and aspect_setting == "keep_height":
+					viewport.set_size_override(true, Vector2(round(original_resolution.y * native_aspect_ratio), round(original_resolution.y)))
+				else:
+					viewport.set_size_override(true, original_resolution)
+			elif usage == USAGE_3D:
+				if aspect_diff > 1.0 + epsilon:
+					viewport.set_size_override(true, Vector2(round(original_resolution.x * aspect_diff), round(original_resolution.y)))
+				elif aspect_diff < 1.0 - epsilon:
+					viewport.set_size_override(true, Vector2(round(original_resolution.x), round(original_resolution.y / aspect_diff)))
+			
+func set_sampler_size():
+	if overlay:
+		var stretch_setting = get_stretch_setting()
+		var aspect_setting = get_aspect_setting()
+		var aspect_diff = native_aspect_ratio / original_aspect_ratio
+		if usage == USAGE_2D:
+			if aspect_diff < 1.0 - epsilon and aspect_setting == "keep_width":
+				overlay.rect_size = Vector2(round(original_resolution.x), round(original_resolution.x / native_aspect_ratio))
+			elif aspect_diff > 1.0 + epsilon and aspect_setting == "keep_height":
+				overlay.rect_size = Vector2(round(original_resolution.y * native_aspect_ratio), round(original_resolution.y))
+			else:
+				overlay.rect_size = Vector2(round(original_resolution.x), round(original_resolution.y))
+		elif usage == USAGE_3D:
+			overlay.rect_size = Vector2(round(native_resolution.x), round(native_resolution.y))
+			if aspect_diff > 1.0 + epsilon:
+				overlay.rect_size.x = round(native_resolution.y * original_aspect_ratio)
+			elif aspect_diff < 1.0 - epsilon:
+				overlay.rect_size.y = round(native_resolution.x / original_aspect_ratio)
+		var overlay_size = overlay.rect_size
+		var screen_size = Vector2(0.0, 0.0)
+		if usage == USAGE_2D:
+			screen_size = original_resolution
+		elif usage == USAGE_3D:
+			screen_size = native_resolution
+		if stretch_setting == "disabled" or usage == USAGE_2D:
+			if aspect_setting == "keep":
+				overlay.rect_position.x = 0
+				overlay.rect_position.y = 0
+			elif aspect_setting == "keep_width" or aspect_setting == "keep_height":
+				overlay.rect_position.x = 0
+				overlay.rect_position.y = 0
+				if usage == USAGE_3D:
+					if aspect_diff > 1.0 + epsilon:
+						overlay.rect_position.x = round((screen_size.x * aspect_diff - overlay_size.x) * 0.5)
+					elif aspect_diff < 1.0 - epsilon:
+						overlay.rect_position.y = round((screen_size.y / aspect_diff - overlay_size.y) * 0.5)
+			elif aspect_setting == "expand":
+				if usage == USAGE_3D:
+					overlay.rect_size = screen_size
+				elif aspect_diff > 1.0 + epsilon:
+					overlay.rect_size = Vector2(round(screen_size.x * aspect_diff), round(screen_size.y))
+				elif aspect_diff < 1.0 - epsilon:
+					overlay.rect_size = Vector2(round(screen_size.x), round(screen_size.y / aspect_diff))
+				else:
+					overlay.rect_size = screen_size
+			elif aspect_setting == "ignore":
+				if usage == USAGE_3D:
+					overlay.rect_size = screen_size
+		elif stretch_setting == "viewport":
+			overlay.rect_size = native_resolution
+		elif stretch_setting == "2d":
+			overlay.rect_size = original_resolution
+			overlay_size = overlay.rect_size
+			overlay.rect_position.x = 0
+			overlay.rect_position.y = 0
+			if aspect_setting == "expand":
+				if aspect_diff > 1.0 + epsilon:
+					overlay.rect_size = Vector2(round(original_resolution.y * native_aspect_ratio), round(original_resolution.y))
+				elif aspect_diff < 1.0 - epsilon:
+					overlay.rect_size = Vector2(round(original_resolution.x), round(original_resolution.x / native_aspect_ratio))
+			elif aspect_setting == "keep_width":
+				overlay.rect_position.x = 0.0
+				if aspect_diff < 1.0 - epsilon:
+					overlay.rect_position.y = round((overlay_size.y / aspect_diff - overlay_size.y) * 0.5)
+			elif aspect_setting == "keep_height":
+				overlay.rect_position.y = 0.0
+				if aspect_diff > 1.0 + epsilon:
+					overlay.rect_position.x = round((overlay_size.x * aspect_diff - overlay_size.x) * 0.5)
+				
+func change_scale_factor(val):
+	scale_factor = val
+	on_window_resize()
+	
+func change_smoothness(val):
+	smoothness = val
+	if sampler_material:
+		sampler_material.set_shader_param("smoothness", smoothness)
+		
+func change_msaa(val):
+	msaa = val
+	if viewport:
+		viewport.msaa = msaa
+		
+func change_fxaa(val):
+	fxaa = val
+	if viewport:
+		viewport.fxaa = fxaa
+		
+func change_shadow_atlas(val):
+	shadow_atlas = val
+	
+func on_window_resize():
+	get_screen_size()
+	set_viewport_size()
+	resize_viewport()
+	scale_viewport_canvas()
+	set_shader_resolution()
+	set_sampler_size()
+	
+func get_aspect_setting():
+	return ProjectSettings.get_setting("display/window/stretch/aspect")
+	
+func get_stretch_setting():
+	return ProjectSettings.get_setting("display/window/stretch/mode")
+	
+func _input(event):
+	if viewport:
+		viewport.input(event)
+		viewport.unhandled_input(event)
